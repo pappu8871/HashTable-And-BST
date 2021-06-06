302985f50014bb1a9eb768c9b666cbf21c65c0f7
your username <59005110+pappu8871@users.noreply.github.com>
2021-06-06 20:37:20 +0530

    Added:Frequency of words in a sentence

---
+++ HashTable.java
@@ -0,0 +1,32 @@
+package com.hashTable;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class HashTableUC1 {
+
+	static void count (String str) {
+		Map<String,Integer> map = new TreeMap<>();
+
+		String arr[] = str.split(" ");
+		for (int i = 0;i<arr.length;i++) {
+			if (map.containsKey(arr[i])) {
+				map.put(arr[1],map.get(arr[i]) + 1);
+			}
+			else {
+				map.put(arr[i], 1);
+			}
+		}
+		for(Map.Entry<String,Integer> entry:
+			map.entrySet()) {
+			System.out.println(entry.getKey() + " -> "+entry.getValue());
+		}
+	}
+
+	public static void main(String[] args) {
+		String str = "To be or not to be";
+		count(str);
+	}
+}
+
+
